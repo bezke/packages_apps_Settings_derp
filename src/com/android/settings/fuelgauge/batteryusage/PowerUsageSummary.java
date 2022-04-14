54759bdfc5cb9efda8a38a974ea57cc193f062db
DennySPb <dennyspb@gmail.com>
2022-04-15 00:09:24 +0530

    Add toggle to switch between AOSP/Google battery graphs
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
@@ -106,6 +106,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             restartBatteryInfoLoader();
+            initPreference();
         }
     };
 
@@ -199,6 +200,10 @@ public class PowerUsageSummary extends PowerUsageBase implements
         super.onResume();
         getContentResolver().registerContentObserver(
                 Global.getUriFor(Global.BATTERY_ESTIMATES_LAST_UPDATE_TIME),
+                false,
+                mSettingsObserver);
+        getContentResolver().registerContentObserver(
+                Settings.System.getUriFor("battery_24_hrs_stats"),
                 false,
                 mSettingsObserver);
         updateSleepModeSummary();
@@ -339,7 +344,12 @@ public class PowerUsageSummary extends PowerUsageBase implements
     @VisibleForTesting
     void initPreference() {
         mBatteryUsagePreference = findPreference(KEY_BATTERY_USAGE);
-        mBatteryUsagePreference.setSummary(getString(R.string.advanced_battery_preference_summary));
+        boolean isChartGraphEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
+                "battery_24_hrs_stats", 0, UserHandle.USER_CURRENT) != 0;
+        mBatteryUsagePreference.setSummary(
+                isChartGraphEnabled ?
+                        getString(R.string.advanced_battery_preference_summary_with_hours) :
+                        getString(R.string.advanced_battery_preference_summary));
 
         mHelpPreference = findPreference(KEY_BATTERY_ERROR);
         mHelpPreference.setVisible(false);
