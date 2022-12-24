44063fd4b8ba17da338f76be1395e27a030845e7
NurKeinNeid <mralexman3000@gmail.com>
2022-12-24 15:13:30 +0100

    Update for T

--- src/com/google/android/settings/fuelgauge/PowerUsageFeatureProviderGoogleImpl.java
+++ src/com/google/android/settings/fuelgauge/PowerUsageFeatureProviderGoogleImpl.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.text.TextUtils;
+import android.util.ArraySet;
 import android.util.SparseIntArray;
 
 import androidx.window.R;
@@ -14,14 +15,12 @@ import com.android.settings.fuelgauge.BatteryHistEntry;
 import com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl;
 import com.android.settingslib.fuelgauge.Estimate;
 import com.android.settingslib.utils.PowerUtil;
-
 import com.google.android.settings.experiments.PhenotypeProxy;
 import com.google.android.settings.fuelgauge.DatabaseUtils;
 
 import java.time.Clock;
 import java.time.Duration;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -52,7 +51,7 @@ public class PowerUsageFeatureProviderGoogleImpl extends PowerUsageFeatureProvid
         this.mPackageManager = packageManager;
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
     public Estimate getEnhancedBatteryPrediction(Context context) {
         long j;
         Cursor query = context.getContentResolver().query(getEnhancedBatteryPredictionUri(), null, null, null, null);
@@ -61,25 +60,26 @@ public class PowerUsageFeatureProviderGoogleImpl extends PowerUsageFeatureProvid
                 if (query.moveToFirst()) {
                     int columnIndex = query.getColumnIndex(BATTERY_ESTIMATE_BASED_ON_USAGE_COL);
                     boolean z = true;
-                    if (!(columnIndex == -1 || 1 == query.getInt(columnIndex))) {
+                    if (columnIndex != -1 && 1 != query.getInt(columnIndex)) {
                         z = false;
                     }
+                    boolean z2 = z;
                     int columnIndex2 = query.getColumnIndex(AVERAGE_BATTERY_LIFE_COL);
                     if (columnIndex2 != -1) {
                         long j2 = query.getLong(columnIndex2);
                         if (j2 != -1) {
-                            long millis = Duration.ofMinutes(15).toMillis();
-                            if (Duration.ofMillis(j2).compareTo(Duration.ofDays(1)) >= 0) {
-                                millis = Duration.ofHours(1).toMillis();
+                            long millis = Duration.ofMinutes(15L).toMillis();
+                            if (Duration.ofMillis(j2).compareTo(Duration.ofDays(1L)) >= 0) {
+                                millis = Duration.ofHours(1L).toMillis();
                             }
                             j = PowerUtil.roundTimeToNearestThreshold(j2, millis);
-                            Estimate estimate = new Estimate(query.getLong(query.getColumnIndex(BATTERY_ESTIMATE_COL)), z, j);
+                            Estimate estimate = new Estimate(query.getLong(query.getColumnIndex(BATTERY_ESTIMATE_COL)), z2, j);
                             query.close();
                             return estimate;
                         }
                     }
                     j = -1;
-                    Estimate estimate2 = new Estimate(query.getLong(query.getColumnIndex(BATTERY_ESTIMATE_COL)), z, j);
+                    Estimate estimate2 = new Estimate(query.getLong(query.getColumnIndex(BATTERY_ESTIMATE_COL)), z2, j);
                     query.close();
                     return estimate2;
                 }
@@ -92,14 +92,14 @@ public class PowerUsageFeatureProviderGoogleImpl extends PowerUsageFeatureProvid
                 throw th;
             }
         }
-        if (query == null) {
+        if (query != null) {
+            query.close();
             return null;
         }
-        query.close();
         return null;
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
     public SparseIntArray getEnhancedBatteryPredictionCurve(Context context, long j) {
         try {
             Cursor query = context.getContentResolver().query(getEnhancedBatteryPredictionCurveUri(), null, null, null, null);
@@ -122,7 +122,7 @@ public class PowerUsageFeatureProviderGoogleImpl extends PowerUsageFeatureProvid
         }
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
     public boolean isEnhancedBatteryPredictionEnabled(Context context) {
         if (!isTurboEnabled(context)) {
             return false;
@@ -145,12 +145,12 @@ public class PowerUsageFeatureProviderGoogleImpl extends PowerUsageFeatureProvid
         return new Uri.Builder().scheme("content").authority("com.google.android.apps.turbo.estimated_time_remaining").appendPath("discharge_curve").build();
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
     public String getAdvancedUsageScreenInfoString() {
         return this.mContext.getString(R.string.advanced_battery_graph_subtext);
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
     public boolean getEarlyWarningSignal(Context context, String str) {
         Uri.Builder appendPath = new Uri.Builder().scheme("content").authority("com.google.android.apps.turbo.estimated_time_remaining").appendPath("early_warning").appendPath("id");
         if (TextUtils.isEmpty(str)) {
@@ -188,54 +188,60 @@ public class PowerUsageFeatureProviderGoogleImpl extends PowerUsageFeatureProvid
         return PhenotypeProxy.getFlagByPackageAndKey(context, "com.google.android.apps.turbo", "NudgesBatteryEstimates__estimated_time_remaining_provider_enabled", false);
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
     public boolean isChartGraphEnabled(Context context) {
         loadChartConfiguration(context);
         return sChartGraphEnabled;
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
     public boolean isChartGraphSlotsEnabled(Context context) {
         loadChartConfiguration(context);
         return sChartGraphSlotsEnabled;
     }
 
     private void loadChartConfiguration(Context context) {
-        if (!sChartConfigurationLoaded) {
-            boolean isSettingsIntelligenceExist = isSettingsIntelligenceExist(context);
-            boolean z = false;
-            sChartGraphEnabled = isSettingsIntelligenceExist && DatabaseUtils.isContentProviderEnabled(context);
-            if (isSettingsIntelligenceExist && PhenotypeProxy.getFlagByPackageAndKey(context, "com.google.android.settings.intelligence", "BatteryUsage__is_time_slot_supported", false)) {
-                z = true;
-            }
-            sChartGraphSlotsEnabled = z;
-            sChartConfigurationLoaded = true;
+        if (sChartConfigurationLoaded) {
+            return;
         }
+        boolean isSettingsIntelligenceExist = isSettingsIntelligenceExist(context);
+        boolean z = false;
+        sChartGraphEnabled = isSettingsIntelligenceExist && DatabaseUtils.isContentProviderEnabled(context);
+        if (isSettingsIntelligenceExist && PhenotypeProxy.getFlagByPackageAndKey(context, "com.google.android.settings.intelligence", "BatteryUsage__is_time_slot_supported", false)) {
+            z = true;
+        }
+        sChartGraphSlotsEnabled = z;
+        sChartConfigurationLoaded = true;
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
+    public Intent getResumeChargeIntent() {
+        return new Intent(ACTION_RESUME_CHARGING).setPackage(PACKAGE_NAME_SYSTEMUI).addFlags(1342177280);
+    }
+
+    @Override
     public Map<Long, Map<String, BatteryHistEntry>> getBatteryHistory(Context context) {
         return DatabaseUtils.getHistoryMap(context, Clock.systemUTC(), true);
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
     public Uri getBatteryHistoryUri() {
         return DatabaseUtils.BATTERY_CONTENT_URI;
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
     public Set<CharSequence> getHideBackgroundUsageTimeSet(Context context) {
-        HashSet hashSet = new HashSet();
-        Collections.addAll(hashSet, context.getResources().getTextArray(R.array.allowlist_hide_background_in_battery_usage));
-        return hashSet;
+        ArraySet arraySet = new ArraySet();
+        Collections.addAll(arraySet, context.getResources().getTextArray(R.array.allowlist_hide_background_in_battery_usage));
+        return arraySet;
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
     public CharSequence[] getHideApplicationEntries(Context context) {
         return context.getResources().getTextArray(R.array.allowlist_hide_entry_in_battery_usage);
     }
 
-    @Override // com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl, com.android.settings.fuelgauge.PowerUsageFeatureProvider
+    @Override
     public CharSequence[] getHideApplicationSummary(Context context) {
         return context.getResources().getTextArray(R.array.allowlist_hide_summary_in_battery_usage);
     }
