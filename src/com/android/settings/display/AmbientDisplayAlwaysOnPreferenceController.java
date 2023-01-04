LeafOS-Project/android_packages_apps_Settings

Settings: Support custom device doze settings
* Like https://review.lineageos.org/c/LineageOS/android_packages_apps_Settings/+/318590
  but in an AOSP compatible way

Change-Id: I694987dc05cfb6fee53329252031d805bd52c012

src/com/android/settings/display/AmbientDisplayAlwaysOnPreferenceController.java
@@ -29,6 +29,8 @@ import androidx.preference.Preference;
 import com.android.settings.R;
 import com.android.settings.core.TogglePreferenceController;
 
+import org.leafos.settings.util.PackageManagerUtils;
+
 public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreferenceController {
 
     private final int ON = 1;
@@ -47,7 +49,8 @@ public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreference
     @Override
     public int getAvailabilityStatus() {
         return isAvailable(getConfig())
-                && !SystemProperties.getBoolean(PROP_AWARE_AVAILABLE, false) ?
+                && !SystemProperties.getBoolean(PROP_AWARE_AVAILABLE, false)
+                && !PackageManagerUtils.isCustomDozePresent(mContext.getPackageManager()) ?
                 AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 

src/com/android/settings/display/AmbientDisplayNotificationsPreferenceController.java
@@ -30,6 +30,8 @@ import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
 
+import org.leafos.settings.util.PackageManagerUtils;
+
 public class AmbientDisplayNotificationsPreferenceController extends
         TogglePreferenceController implements Preference.OnPreferenceChangeListener {
 
@@ -81,6 +83,7 @@ public class AmbientDisplayNotificationsPreferenceController extends
     @Override
     public int getAvailabilityStatus() {
         return getAmbientConfig().pulseOnNotificationAvailable()
+                && !PackageManagerUtils.isCustomDozePresent(mContext.getPackageManager())
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 

src/com/android/settings/display/LineageDozePreferenceController.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2022 The LeafOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+
+import com.android.settings.core.BasePreferenceController;
+
+import org.leafos.settings.util.PackageManagerUtils;
+
+public class LineageDozePreferenceController extends BasePreferenceController {
+
+    public LineageDozePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return PackageManagerUtils.isCustomDozePresent(mContext.getPackageManager()) ?
+                AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return "";
+    }
+}

src/org/leafos/settings/util/PackageManagerUtils.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2022 The LeafOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.leafos.settings.util;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.UserHandle;
+
+public class PackageManagerUtils {
+
+    public static String ACTION_DOZE = "org.lineageos.settings.device.DOZE_SETTINGS";
+
+    public static boolean isIntentPresent(PackageManager pm, String name) {
+        Intent intent = new Intent(name);
+        for (ResolveInfo info : pm.queryIntentActivitiesAsUser(intent,
+                PackageManager.MATCH_SYSTEM_ONLY, UserHandle.myUserId())) {
+            if (info.activityInfo != null) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isCustomDozePresent(PackageManager pm) {
+        return isIntentPresent(pm, ACTION_DOZE);
+    }
+
+}

bezke/packages_apps_Settings_derp

src/com/android/settings/display/AmbientDisplayAlwaysOnPreferenceController.java
@@ -29,6 +29,8 @@ import androidx.preference.Preference;
 import com.android.settings.R;
 import com.android.settings.core.TogglePreferenceController;
 
+import org.derpfest.settings.util.PackageManagerUtils;
+
 public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreferenceController {
 
     private final int ON = 1;
@@ -47,7 +49,8 @@ public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreference
     @Override
     public int getAvailabilityStatus() {
         return isAvailable(getConfig())
-                && !SystemProperties.getBoolean(PROP_AWARE_AVAILABLE, false) ?
+                && !SystemProperties.getBoolean(PROP_AWARE_AVAILABLE, false)
+                && !PackageManagerUtils.isCustomDozePresent(mContext.getPackageManager()) ?
                 AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 

src/com/android/settings/display/AmbientDisplayNotificationsPreferenceController.java
@@ -30,6 +30,8 @@ import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
 
+import org.derpfest.settings.util.PackageManagerUtils;
+
 public class AmbientDisplayNotificationsPreferenceController extends
         TogglePreferenceController implements Preference.OnPreferenceChangeListener {
 
@@ -81,6 +83,7 @@ public class AmbientDisplayNotificationsPreferenceController extends
     @Override
     public int getAvailabilityStatus() {
         return getAmbientConfig().pulseOnNotificationAvailable()
+                && !PackageManagerUtils.isCustomDozePresent(mContext.getPackageManager())
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 

src/com/android/settings/display/LineageDozePreferenceController.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2022 The LeafOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+
+import com.android.settings.core.BasePreferenceController;
+
+import org.derpfest.settings.util.PackageManagerUtils;
+
+public class LineageDozePreferenceController extends BasePreferenceController {
+
+    public LineageDozePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return PackageManagerUtils.isCustomDozePresent(mContext.getPackageManager()) ?
+                AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return "";
+    }
+}

src/org/derpfest/settings/util/PackageManagerUtils.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2022 The LeafOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.settings.util;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.UserHandle;
+
+public class PackageManagerUtils {
+
+    public static String ACTION_DOZE = "org.lineageos.settings.device.DOZE_SETTINGS";
+
+    public static boolean isIntentPresent(PackageManager pm, String name) {
+        Intent intent = new Intent(name);
+        for (ResolveInfo info : pm.queryIntentActivitiesAsUser(intent,
+                PackageManager.MATCH_SYSTEM_ONLY, UserHandle.myUserId())) {
+            if (info.activityInfo != null) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isCustomDozePresent(PackageManager pm) {
+        return isIntentPresent(pm, ACTION_DOZE);
+    }
+
+}