LMODroid/platform_packages_apps_Settings

PackageListAdapter: Mirror preference_icon with lineage changes
Change-Id: Ibc038384fec6f09122ece1af32be709fd0fe9040

src/com/android/settings/libremobileos/widget/PackageListAdapter.java
@@ -118,7 +118,7 @@ public class PackageListAdapter extends BaseAdapter implements Runnable {
         if (convertView != null) {
             holder = (ViewHolder) convertView.getTag();
         } else {
-            convertView = mInflater.inflate(R.layout.preference_icon, null, false);
+            convertView = mInflater.inflate(R.layout.preference_icon_lmo, null, false);
             holder = new ViewHolder();
             convertView.setTag(holder);
             holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);

bezke/packages_apps_Settings_derp

src/com/android/settings/derp/widget/PackageListAdapter.java
@@ -118,7 +118,7 @@ public class PackageListAdapter extends BaseAdapter implements Runnable {
         if (convertView != null) {
             holder = (ViewHolder) convertView.getTag();
         } else {
-            convertView = mInflater.inflate(R.layout.preference_icon, null, false);
+            convertView = mInflater.inflate(R.layout.preference_icon_derp, null, false);
             holder = new ViewHolder();
             convertView.setTag(holder);
             holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);