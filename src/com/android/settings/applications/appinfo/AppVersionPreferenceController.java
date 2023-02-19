mukul2259/android_packages_apps_Settings

Settings: Update package details to match S style
 * After: http://i.imgur.com/jcvNT2R.png

Change-Id: I855e8d8a1618283bd99899580e9077bc362647dc

src/com/android/settings/applications/appinfo/AppVersionPreferenceController.java
@@ -36,7 +36,7 @@ public class AppVersionPreferenceController extends AppInfoPreferenceControllerB
         if (packageInfo == null) {
             return null;
         }
-        return mContext.getString(R.string.version_text,
+        return mContext.getString(R.string.p404_version_text,
                 BidiFormatter.getInstance().unicodeWrap(packageInfo.versionName));
     }
 }

bezke/packages_apps_Settings_derp

src/com/android/settings/applications/appinfo/AppVersionPreferenceController.java
@@ -36,7 +36,7 @@ public class AppVersionPreferenceController extends AppInfoPreferenceControllerB
         if (packageInfo == null) {
             return null;
         }
-        return mContext.getString(R.string.version_text,
+        return mContext.getString(R.string.package_version_text,
                 BidiFormatter.getInstance().unicodeWrap(packageInfo.versionName));
     }
 }