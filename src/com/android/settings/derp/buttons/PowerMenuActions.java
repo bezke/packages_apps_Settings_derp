LMODroid/platform_packages_apps_Settings

Add a toggle for panic power button option
Change-Id: I95deef3c98803b8f70403d3abb45eb65895a1dda

src/com/android/settings/libremobileos/buttons/PowerMenuActions.java
@@ -36,6 +36,7 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.settingslib.applications.ServiceListing;
 
 import com.android.internal.util.libremobileos.PowerMenuConstants;
+import com.android.internal.util.libremobileos.PowerMenuUtils;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -62,6 +63,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private CheckBoxPreference mBugReportPref;
     private CheckBoxPreference mEmergencyPref;
     private CheckBoxPreference mDeviceControlsPref;
+    private CheckBoxPreference mPanicPref;
 
     private LineageGlobalActions mLineageGlobalActions;
 
@@ -97,6 +99,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 mEmergencyPref = findPreference(GLOBAL_ACTION_KEY_EMERGENCY);
             } else if (action.equals(GLOBAL_ACTION_KEY_DEVICECONTROLS)) {
                 mDeviceControlsPref = findPreference(GLOBAL_ACTION_KEY_DEVICECONTROLS);
+            } else if (action.equals(GLOBAL_ACTION_KEY_PANIC)) {
+                mPanicPref = findPreference(GLOBAL_ACTION_KEY_PANIC);
             }
         }
 
@@ -162,6 +166,12 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             serviceListing.reload();
         }
 
+        if (mPanicPref != null) {
+            mPanicPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_PANIC));
+            mPanicPref.setEnabled(PowerMenuUtils.isPanicAvailable(mContext));
+        }
+
         updatePreferences();
     }
 
@@ -200,6 +210,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         } else if (preference == mDeviceControlsPref) {
             value = mDeviceControlsPref.isChecked();
             mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_DEVICECONTROLS);
+
+        } else if (preference == mPanicPref) {
+            value = mPanicPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_PANIC);
 
         } else {
             return super.onPreferenceTreeClick(preference);

bezke/packages_apps_Settings_derp

src/com/android/settings/derp/buttons/PowerMenuActions.java
@@ -35,6 +35,7 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.settingslib.applications.ServiceListing;
 
 import com.android.internal.util.derp.PowerMenuConstants;
+import com.android.internal.util.derp.PowerMenuUtils;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -62,6 +63,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private CheckBoxPreference mBugReportPref;
     private CheckBoxPreference mEmergencyPref;
     private CheckBoxPreference mDeviceControlsPref;
+    private CheckBoxPreference mPanicPref;
 
     private LineageGlobalActions mLineageGlobalActions;
 
@@ -96,6 +98,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 mEmergencyPref = findPreference(GLOBAL_ACTION_KEY_EMERGENCY);
             } else if (action.equals(GLOBAL_ACTION_KEY_DEVICECONTROLS)) {
                 mDeviceControlsPref = findPreference(GLOBAL_ACTION_KEY_DEVICECONTROLS);
+            } else if (action.equals(GLOBAL_ACTION_KEY_PANIC)) {
+                mPanicPref = findPreference(GLOBAL_ACTION_KEY_PANIC);
             }
         }
 
@@ -161,6 +165,12 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             serviceListing.reload();
         }
 
+        if (mPanicPref != null) {
+            mPanicPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_PANIC));
+            mPanicPref.setEnabled(PowerMenuUtils.isPanicAvailable(mContext));
+        }
+
         updatePreferences();
     }
 
@@ -199,6 +209,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         } else if (preference == mDeviceControlsPref) {
             value = mDeviceControlsPref.isChecked();
             mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_DEVICECONTROLS);
+
+        } else if (preference == mPanicPref) {
+            value = mPanicPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_PANIC);
 
         } else {
             return super.onPreferenceTreeClick(preference);